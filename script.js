ca05dcb3ca74023634933919b3dbe2e36893d5e0
Alex <alex.dev090283@gmail.com>
2020-03-19 14:01:02 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+'use strict';
+
+var leftBorderWidth = 1;
+let second = 2;
+const pi = 3.14;
+
+console.log(pi);
+
+let persone = {
+    name: "John",
+    age: 25,
+    isMaried: false
+};
+
+console.log(persone["name"]);
+
+//alert("Hellow World!!!");
+
+// let answer = confirm("Are you here?");
+
+// console.log(answer);
+let answer = prompt("Вам есть 18 лет?", "Да");
+
+console.log(answer);
